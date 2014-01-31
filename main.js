51adc41a566515fe6ae113451c99a3fb64359c33
Hideaki Takahashi <mymelo@gmail.com>
2014-01-31 07:51:20 -0500

    added voiceName:native to specify native TTS engine

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ chrome.browserAction.onClicked.addListener(function(tab) {
             {
                 'lang':'ja-JP',
                 'rate':2.0,
+                'voiceName': 'native', // use Native TTS
                 'onEvent':function (event) {
                     console.log(event.type);
                     console.log(event.charIndex);
@@ -28,33 +29,3 @@ chrome.browserAction.onClicked.addListener(function(tab) {
            });
     });
 });
-
-chrome.extension.onMessage.addListener(function(req, sender, sendResponse) {
-console.log('onMessage');
-        chrome.tts.getVoices(
-            function (voices) {
-                for (var i = 0; i < voices.length; i++) {
-                    console.log('Voice ' + i + ':');
-                    console.log('  name: ' + voices[i].voiceName);
-                    console.log('  lang: ' + voices[i].lang);
-                    console.log('  gender: ' + voices[i].gender);
-                    console.log('  extension id: ' + voices[i].extensionId);
-                    console.log('  event types: ' + voices[i].eventTypes);
-                }
-            });
-        var txt = req.data || '今アクティブなタブは<break />' + tab.title + 'です';
-        chrome.tts.speak(
-            '<?xml version="1.0"?><speak>' + txt + '</speak>',
-            {
-                'lang':'ja-JP',
-                'rate':2.0,
-                'onEvent':function (event) {
-                    console.log(event.type);
-                    console.log(event.charIndex);
-                    console.log(event);
-                }
-            },
-            function () {
-                console.log('Error: ' + chrome.extension.lastError);
-           });
-    });
