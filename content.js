716a24222378ebd0dc5f56fad103220aaacc4952
Hideaki Takahashi <mymelo@gmail.com>
2014-01-31 07:39:17 -0500

    updated deprecated API usage

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-chrome.extension.onMessage.addListener(function(req, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function(req, sender, sendResponse) {
     if (req.method === "getSelectionText") {
         sendResponse({data: document.getSelection().toString()});
     } else {
